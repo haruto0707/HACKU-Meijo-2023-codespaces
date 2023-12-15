16be543fad0a15b7d65c6c35e35349d518c674a2
haruto0707 <221205070@ccmailg.meijo-u.ac.jp>
2023-12-15 20:50:21 +0900

    first integration

--- app/src/main/java/com/raywenderlich/android/sleepguardian/receiver/SleepReceiver.kt
+++ app/src/main/java/com/raywenderlich/android/sleepguardian/receiver/SleepReceiver.kt
@@ -1,69 +1,57 @@
-/*
- * Copyright (c) 2021 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * This project and source code may use libraries or frameworks that are
- * released under various Open-Source licenses. Use of those libraries and
- * frameworks are governed by their own individual licenses.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.raywenderlich.android.sleepguardian.receiver
 
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import com.google.android.gms.location.SleepClassifyEvent
 import com.google.android.gms.location.SleepSegmentEvent
+import com.raywenderlich.android.sleepguardian.AlarmService
+import java.util.concurrent.TimeUnit
 
 class SleepReceiver : BroadcastReceiver() {
 
   override fun onReceive(context: Context, intent: Intent) {
+    if ("ACTION_ALARM_STOPPED" == intent.action) {
+      // ブロードキャストからboolean型の値を取得
+      val isAlarmStopped = intent.getBooleanExtra("isAlarmStopped", false)
 
-    if (SleepSegmentEvent.hasEvents(intent)) {
+      if (isAlarmStopped) {
+        if (SleepSegmentEvent.hasEvents(intent)) {
+          val events = SleepSegmentEvent.extractEvents(intent)
 
-    } else if (SleepClassifyEvent.hasEvents(intent)) {
-      val events = SleepClassifyEvent.extractEvents(intent)
+          Log.d(TAG, "Logging SleepSegmentEvents")
+          for (event in events) {
+            Log.d(TAG,
+              "${event.startTimeMillis} to ${event.endTimeMillis} with status ${event.status}")
+          }
+        } else if (SleepClassifyEvent.hasEvents(intent)) {
+          val events = SleepClassifyEvent.extractEvents(intent)
 
-      Log.d(TAG, "Logging SleepClassifyEvents with Motion:")
-      val sendIntent = Intent("custom.action")
-      for (event in events) {
-        Log.d(TAG,
-          "Confidence: ${event.confidence} - Light: ${event.light} - Motion: ${event.motion}")
-        sendIntent.putExtra("key", event.motion)
+          Log.d(TAG, "Logging SleepClassifyEvents")
+          for (event in events) {
+            Log.d(TAG,
+              "Confidence: ${event.confidence} - Light: ${event.light} - Motion: ${event.motion}")
 
-        // 別のReceiverにデータを送信
-        context.sendBroadcast(sendIntent)
+            if (event.motion == 1) {
+              scheduleAlarm(context)
+            }
+          }
+        }
       }
     }
   }
 
+  private fun scheduleAlarm(context: Context) {
+    val handler = Handler(Looper.getMainLooper())
+    handler.postDelayed({
+      val alarmIntent = Intent(context, AlarmService::class.java)
+      context.startService(alarmIntent)
+    }, TimeUnit.MINUTES.toMillis(5)) // 5分後にスケジュール、必要に応じて調整してください
+  }
 
   companion object {
 
@@ -71,9 +59,12 @@ class SleepReceiver : BroadcastReceiver() {
 
     fun createPendingIntent(context: Context): PendingIntent {
       val intent = Intent(context, SleepReceiver::class.java)
-
-      return PendingIntent.getBroadcast(context,
-          0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+      return PendingIntent.getBroadcast(
+        context,
+        0,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+      )
     }
   }
 }
--- app/src/main/java/com/raywenderlich/android/sleepguardian/receiver/StopButtonReceiver.kt
+++ app/src/main/java/com/raywenderlich/android/sleepguardian/receiver/StopButtonReceiver.kt
@@ -3,8 +3,12 @@ package com.raywenderlich.android.sleepguardian.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import com.raywenderlich.android.sleepguardian.AlarmService
+import com.raywenderlich.android.sleepguardian.receiver.SleepReceiver
+import java.util.concurrent.TimeUnit
 
 class StopButtonReceiver : BroadcastReceiver() {
 
@@ -12,19 +16,16 @@ class StopButtonReceiver : BroadcastReceiver() {
         // アラームを停止するための処理
         stopAlarm(context)
 
-        // 5分後に SleepReceiver を起動
-        scheduleSleepReceiver(context, intent)
     }
 
     private fun stopAlarm(context: Context?) {
         // アラーム停止の処理を追加
         val stopIntent = Intent(context, AlarmService::class.java)
         context?.stopService(stopIntent)
-    }
 
-    private fun scheduleSleepReceiver(context: Context?, intent: Intent?) {
-        val message = intent?.getStringExtra("key")
-        // motion の値をログに出力
-        Log.d("StopButtonReceiver", "スリープレシーバーを1分後にスケジュールしました。 $message")
+        // boolean型の値をSleepReceiverに送信
+        val broadcastIntent = Intent("ACTION_ALARM_STOPPED")
+        broadcastIntent.putExtra("isAlarmStopped", true)
+        context?.sendBroadcast(broadcastIntent)
     }
 }
